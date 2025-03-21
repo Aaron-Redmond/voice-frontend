1ce4fcb105deb5a3c3b910b8a4bd2224bb04397c
Aaron-Redmond <aaron.redmond@outlook.com>
2025-03-21 10:46:09 +1000

    set nextjs build to be stand alone

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'standalone',
+  // This ensures better compatibility when running in a container
+  experimental: {
+    // Enable if you're using middleware or edge functions
+    // serverActions: true,
+  }
+};
 
 export default nextConfig;
