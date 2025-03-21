bfc98323dacfc37eaf7b7c996bbed5bfac13e674
Aaron-Redmond <aaron.redmond@outlook.com>
2025-03-21 13:39:26 +1000

    set the base path

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  basePath: '/voice',
+  assetPrefix: '/voice',
   // This ensures better compatibility when running in a container
   experimental: {
     // Enable if you're using middleware or edge functions
